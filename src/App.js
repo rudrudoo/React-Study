80e8430cf9db99560a980451a6cd0b9660ce4b31
rudrudoo <khun2711@gmail.com>
2024-02-04 17:38:04 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">      
-      <img src={logo} alt="logo" />
-      <h2>리액트 공부하기 익숙해지기</h2>    
-      <h2>git 은 또 뭐야</h2>
-    </div>
-  );
+const customers = [
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '920915',
+  'gender' : '남자',
+  'job' : '백수'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '김똥개',
+  'birthday' : '920102',
+  'gender' : '남자',
+  'job' : '백수'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '김철수',
+  'birthday' : '920318',
+  'gender' : '남자',
+  'job' : '백수'
+}
+]
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        {
+          customers.map(d =>{
+            return (
+              <Customer
+                id={d.id}
+                image={d.image}
+                name={d.name}
+                birthday={d.birthday}
+                gender={d.gender}
+                job={d.job}
+              />      
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
